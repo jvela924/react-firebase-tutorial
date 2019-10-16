be81b215cb0bf08789e6c5fead7b4a45a8daafa0
Jeremy Vela <jvela924@gmail.com>
2019-10-16 14:55:56 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/layout/navbar.js'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/navbar'
+import Dashboard from './components/dashboard/dashboard.js'
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path='/'component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
